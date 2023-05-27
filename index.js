ab21f8178388f6f6080676b52363208c5ee2de43
Rarij R <rarijr5144@gmail.com>
2023-05-27 17:31:06 +0530

    Implemented a clear and reset button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let count = 0
-let saveEl = document.getElementById('save-el').innerText;
+let saveEl = document.getElementById('save-el');
 
 function increment(){
     count++;
@@ -7,7 +7,18 @@ function increment(){
 }
 
 function save(){
-    let result =" "+count+ " - "
-    saveEl += result;
-    document.getElementById('save-el').innerText = saveEl
+    let result =" "+count+ " , "
+    saveEl.innerText += result;
+    count = 0;
+    document.getElementById('js-people').innerText = count;
+}
+
+function reset(){
+    saveEl.innerText = 'Previous Entities :'
+}
+
+function clearup(){
+    count = 0;
+    console.log(count);
+    document.getElementById('js-people').innerText = count;
 }
